402ae6495f3acb97c8849629bfaceaa24b2d5bd7
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2023-03-01 09:37:49 -0700

    Add files via upload

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+export const UPDATE = 'UPDATE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const LOGIN='LOGIN';
+export const CREATE='CREATE';
+export const UPDATERULES="UPDATERULES";
+export const UPDATESERVER="UPDATESERVER";
+export const DELETESERVER="DELETESERVER";
